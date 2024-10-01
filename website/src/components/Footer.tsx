e4cba9e43bb1ac441c4714469c4b9ab8f1e51837
Alex Ren <alexren@hackclub.com>
2024-10-01 17:05:58 -0400

    updated footer

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="bg-slate-900 py-12 text-slate-600 text-center">
-                <p>Made with ❤️ by @alexren </p>
+            <div className="bg-slate-900 py-12 text-slate-600 text-center font-mono">
+                <p>Made with ❤️ by <a href="https://github.com/qcoral" target="_blank" className="text-cyan-600">@alexren//qcoral</a> </p>
             </div>
 
         </footer>
