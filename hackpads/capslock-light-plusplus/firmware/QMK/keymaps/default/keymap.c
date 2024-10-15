df45e58fa144f8d39cf2e743d437238d2657f5e4
UnnaturalTwilight <107954129+UnnaturalTwilight@users.noreply.github.com>
2024-10-15 11:05:10 -0400

    Disableing RGB and comenting out the code for em I lack the skill for SMD soldering so no leds for me :( PCB changes to come

--- hackpads/capslock-light-plusplus/firmware/QMK/keymaps/default/keymap.c
+++ hackpads/capslock-light-plusplus/firmware/QMK/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM encoder_map[][1][2] = {
     [1] = { ENCODER_CCW_CW(KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN) }
     
 };
-
+/*
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {1, 1, HSV_RED}
 );
@@ -57,35 +57,36 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer,
     my_layer1_layer
 );
-
+*/
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_P(PSTR("FN Keys\n"), false);
-            break;
-        case _UNI:
-            oled_write_P(PSTR("Unicode Characters\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
 
     // Acualy display capslock status, the whole point of this board
     led_t led_state = host_keyboard_led_state();
     oled_write_P(led_state.caps_lock ? PSTR("CAPS ") : PSTR("     "), false);
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
     oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    oled_write_ln("\n", false);
+
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("FN Keys"), false);
+            break;
+        case _UNI:
+            oled_write_P(PSTR("Unicode Characters"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Undefined"), false);
+    }
     
     return false;
 }
 #endif
-
+/*
 bool led_update_user(led_t led_state) {
     rgblight_set_layer_state(0, led_state.caps_lock);
     return true;
 }
+*/
