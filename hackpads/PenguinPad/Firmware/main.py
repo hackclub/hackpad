524f48922ef9efd406dce0db7e5a74f1345bd139
James <jamesst@seycove.ca>
2024-10-21 22:36:57 -0700

    yay new code

--- hackpads/PenguinPad/Firmware/main.py
+++ hackpads/PenguinPad/Firmware/main.py
@@ -1,45 +1,57 @@
-#maybe incomplete btw
 print("Starting")
 
-#import stuff
-
 import board
-
-from kmk.kmk_keyboard import KMKKeyboard
+from kmk.kmkkeyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
-
-#innit stuff
-
 from kmk.modules.macros import Macros
-
 from kmk.modules.encoder import EncoderHandler
-encoder_handler = EncoderHandler()
-keyboard.modules = [layers, holdtap, encoder_handler]
+from kmk.extensions.display.ssd1306 import SSD1306
+import busio
 
-macros = Macros()
-keyboard.modules.append(macros)
-
+#Initialize the keyboard
 keyboard = KMKKeyboard()
 
-keyboard.col_pins = (board.A0, board.A1,board.A3,board.A2)
-keyboard.row_pins = (board.A7,board.A8)
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
+#Initialize modules
+encoderhandler = EncoderHandler()
+macros = Macros()
 
-encoder_handler.pins = ((board.A9, board.A10, board.A3, True),)
+#Initialize the I2C bus for the OLED display
+i2c = busio.I2C(scl=board.GP7, sda=board.GP6)
 
-keyboard.keymap = [
-    [
-    KC.Macro(macro), KC.A, KC.B, KC.C,
-    KC.D, KC.E, KC.F, KC.G
-    ]
+#Initialize the SSD1306 display
+display = SSD1306(i2c=i2c, width=128, height=32)
+
+#Add modules and extensions to the keyboard
+keyboard.modules = [macros, encoderhandler]
+keyboard.extensions.append(display)
+
+#Define the pins
+keyboard.colpins = (board.GP26, board.GP27, board.GP28)
+keyboard.rowpins = (board.GP29, board.GP0)
+keyboard.diodeorientation = DiodeOrientation.COL2ROW
+
+#Define encoder pins and map
+encoderhandler.pins = ((board.GP9, board.GP10, board.GP3, True),)
+encoderhandler.map = [
+    ((KC.UP, KC.DOWN, KC.MUTE),),  # Standard
 ]
- 
-encoder_handler.map = [ 
-    ((KC.UP, KC.DOWN, KC.MUTE),), # Standard
-    ]
 
-if __name__ == '__main__':
+#Define the keymap with multiple layers
+keyboard.keymap = [
+    # Layer 0
+    [
+        KC.A, KC.B, KC.C, KC.D,  # 1: A / B / C / D
+        KC.MO(1), KC.MO(2),  # 2: Toggle Layer 1 / 2
+        KC.MPLY, KC.MPLY,  # 3: Play/Pause
+    ],
+]
+
+#Function to update the OLED display with the current layer
+def updatedisplay():
+    display.clear()
+    display.text(f"Layer: {keyboard.active_layers[0]}", 0, 0, 1)
+    display.show()
+
+if __name == '__main':
     keyboard.go()
-
-#temp
