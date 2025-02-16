e3271c452666ed44f52000f496586d1ac5bb2322
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2025-02-16 18:11:20 +0530

    Updated readme to match guidelines

--- hackpads/nimit/README.md
+++ hackpads/nimit/README.md
@@ -9,7 +9,9 @@
 - 1x SSD1306 (0.96")
 - SeeedStudio XIAO RP2040
 - 1x Case (Prusament Lime)
-
+'
+# About:
+Ever since i learnt you can quickly open apps on the taskbar by using the WIN key with the respective number, it's become a staple of my life. The keypad just helps do that a little easier! Additionally, I am a huge music fan so i need some dedicated media control keys. The remaining keys are placeholders for stuff I will setup in the future (Copy paste and save? Dedicated valorant keybinds? idk)
 # Images:
 
 - PCB
--- hackpads/nimit/firmware/qmk/keymaps/default/keymap.c
+++ hackpads/nimit/firmware/qmk/keymaps/default/keymap.c
@@ -21,7 +21,6 @@ const uint16_t PROGMEM encoder_map[][1][2] = {
 
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
-    // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
@@ -29,11 +28,9 @@ bool oled_task_user(void) {
             oled_write_P(PSTR("Default\n"), false);
             break;
         default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
             oled_write_ln_P(PSTR("Undefined"), false);
     }
 
-    // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
