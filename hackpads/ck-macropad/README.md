429c743cc8d67b75f34923ede78516d24941ed58
Chaaruhaas Kandregula <chaaskandregula@gmail.com>
2025-02-15 22:40:52 -0800

    added files

---
+++ hackpads/ck-macropad/README.md
@@ -0,0 +1,16 @@
+![hackpad](assets/hackpad.png)
+
+![schematic](assets/schematic.png)
+
+![pcb](assets/pcb.png)
+
+![case](assets/case.png)
+
+# BOM:
+
+- 1x Case
+- 1x PCB
+- 1x XIAO RP2040
+- 4x Kailh White switches (preferably, but any Cherry MX switch should work)
+- 4x Keycaps
+- 1x Soldering iron
---
+++ hackpads/ck-macropad/firmware/boards/lily58/main.py
@@ -0,0 +1,80 @@
+from kb import KMKKeyboard, rgb_pixel_pin
+
+from kmk.extensions.ble_split import BLE_Split
+from kmk.extensions.layers import Layers
+from kmk.extensions.rgb import RGB
+from kmk.keys import KC
+
+keyboard = KMKKeyboard()
+
+# Cleaner key names
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+
+LOWER = KC.MO(1)
+RAISE = KC.MO(2)
+ADJUST = KC.LT(3, KC.SPC)
+
+RGB_TOG = KC.RGB_TOG
+RGB_HUI = KC.RGB_HUI
+RGB_HUD = KC.RGB_HUI
+RGB_SAI = KC.RGB_SAI
+RGB_SAD = KC.RGB_SAD
+RGB_VAI = KC.RGB_VAI
+RGB_VAD = KC.RGB_VAD
+
+# Adding extensions
+rgb = RGB(
+    pixel_pin=rgb_pixel_pin,
+    num_pixels=27,
+    val_limit=100,
+    hue_default=190,
+    sat_default=100,
+    val_default=5,
+)
+
+# TODO Comment one of these on each side
+# Left is 0, Right is 1
+split_side = 0
+split_side = 1
+split = BLE_Split(split_side=split_side)
+
+layers = Layers()
+
+extensions = [layers, split, rgb]
+
+# fmt:off
+keyboard.keymap = [
+    [  #QWERTY
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,
+        KC.LCTL,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                         KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
+        KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,
+                                  KC.LGUI,   LOWER,  ADJUST, XXXXXXX,      XXXXXXX, KC.ENT,   RAISE,  KC.RALT,
+    ],
+    [  #LOWER
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC.ESC,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                         KC.N6,   KC.N7,  KC.N8,   KC.N9,   KC.N0, KC.BSPC,
+        KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT, XXXXXXX, XXXXXXX,
+        KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                   KC.LGUI,   LOWER,  ADJUST, XXXXXXX,      XXXXXXX,  KC.ENT,   RAISE,  KC.RALT,
+    ],
+    [  #RAISE
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC.ESC, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                         KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,
+        KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.MINS,  KC.EQL, KC.LCBR, KC.RCBR, KC.PIPE,  KC.GRV,
+        KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.UNDS, KC.PLUS, KC.LBRC, KC.RBRC, KC.BSLS, KC.TILD,
+                                   KC.LGUI,   LOWER,  ADJUST, XXXXXXX,      XXXXXXX,  KC.ENT,   RAISE,  KC.RALT,
+    ],
+    [  #ADJUST
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                   KC.LGUI,   LOWER,  ADJUST, XXXXXXX,      XXXXXXX,  KC.ENT,   RAISE,  KC.RALT,
+    ]
+]
+# fmt:on
+
+if __name__ == '__main__':
+    keyboard.go()
