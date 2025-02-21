461d96b9fb69e88414dd7bf97a90cfb48891bd7c
Illuminum2 <trost.zureden08@icloud.com>
2025-02-21 21:22:06 +0100

    Update ScrollPad hardware and firmware
    - Change GPIO pin assignments
    - PCB improvements: add mounting holes, LED position, routing
    - Case improvements: add spacer, cover fix, add Skadis holder
    - Update firmware: add LED layer indication, add mouse scrolling keys, change keys

--- hackpads/ScrollPad/firmware/KMK/main.py
+++ hackpads/ScrollPad/firmware/KMK/main.py
@@ -3,19 +3,50 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
-from kmk.modules.macros import Press, Release, Tap, Macros
 from kmk.modules.layers import Layers
-from kmk.modules.dual_role import DualRoleLayerTap # for middle_key layer switching
+from kmk.modules.holdtap import HoldTap
+from kmk.extensions.rgb import RGB
+from kmk.modules.mouse_keys import MouseKeys
 
 keyboard = KMKKeyboard()
+holdtap = HoldTap()
 
-# Macro and layers
-macros = Macros()
-keyboard.modules.append(macros)
-keyboard.modules.append(Layers())
+# pins
+RGB_PIN = board.GPIO6
+# Up>Right>Down>Left>Middle
+PINS = [board.GPIO1, board.GPIO2, board.GPIO3, board.GPIO4, board.GPIO7]
 
-# pins(Up>Right>Down>Left>Middle)
-PINS = [board.GPIO4, board.GPIO2, board.GPIO1, board.GPIO3, board.GPIO26]
+class LayerRGB(RGB):
+    def on_layer_change(self, layer):
+        if layer == 0:
+            self.set_hsv_fill(185, 100, self.val_default)
+        elif layer == 1:
+            self.set_hsv_fill(325, 100, self.val_default)
+        self.show()
+
+rgb = LayerRGB(
+    pixel_pin=RGB_PIN,
+    num_pixels=4,
+    rgb_order=(0, 1, 2),
+    hue_default=0,
+    sat_default=0,
+    val_default=100,
+)
+
+class RGBLayers(Layers):
+    def activate_layer(self, keyboard, layer, idx=None):
+        super().activate_layer(keyboard, layer, idx)
+        rgb.on_layer_change(layer)
+
+    def deactivate_layer(self, keyboard, layer):
+        super().deactivate_layer(keyboard, layer)
+        rgb.on_layer_change(keyboard.active_layers[0])
+
+# add modules and extensions
+keyboard.modules.append(RGBLayers())
+keyboard.modules.append(holdtap)
+keyboard.modules.append(MouseKeys())
+keyboard.extensions.append(rgb)
 
 # no key matrix
 keyboard.matrix = KeysScanner(
@@ -23,19 +54,15 @@ keyboard.matrix = KeysScanner(
     value_when_pressed=False,
 )
 
-middle_key = DualRoleLayerTap(1, KC.ENTER)
+middle_key = KC.LT(1, KC.ENTER)
 
 # 2-layers
 # arrow keys, middle key can active the second layer if held
 # up/down and sound controls
 keyboard.keymap = [
-    [KC.LEFT,    KC.RIGHT, KC.UP,   KC.DOWN, middle_key],
-    [KC.VOLD,    KC.VOLU,  KC.UP,   KC.DOWN, KC.TRNS]
+    [MS_WHLU,    MS_WHLR, MS_WHLD,   MS_WHLL, middle_key],
+    [KC.VOLU,    KC.MNXT,  KC.VOLD,   KC.MPRV, KC.TRNS]
 ]
 
-# start
 if __name__ == '__main__':
     keyboard.go()
-
-# TODO: LED layer indication
-# TODO: Potentially port to QMK for mouse scrolling instead of arrow keys
