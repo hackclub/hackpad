e68eb3bace57eae7a1363e322159248c86d81867
Glitch <97538549+JustAGlitchFL@users.noreply.github.com>
2025-03-04 01:52:48 -0500

    Add files via upload

---
+++ hackpads/CineWheel/firmware/KMK/main.py
@@ -0,0 +1,48 @@
+# You import all the IOs of your board
+import board
+
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.mouse_keys import MouseKeys
+keyboard.extensions.append(MediaKeys())
+# This is the main instance of your keyboard
+keyboard = KMKKeyboard()
+
+# Add the macro extension
+macros = Macros()
+keyboard.modules.append(macros)
+
+encoder_handler = EncoderHandler()
+keyboard.modules.append(encoder_handler)
+keyboard.modules.append(MouseKeys())
+
+encoder_handler.pins = [board.D5, board.D6] #fix this when i get the board, don't fully understand how kmk handles this yet
+encoder_handler.encoder_map = {
+    #alt scrollwheel
+    0: {0: KC.MACRO(Press(KC.LALT), Tap(KC.MW_UP), Release(KC.LALT)), 1: KC.MACRO(Press(KC.LALT), Tap(KC.MW_DOWN), Release(KC.LALT)),},
+    1: {0: KC.MACRO("Hello world!"), 1: KC.MACRO("Hello world!")},
+}
+
+# Define your pins here!
+PINS = [board.D3, board.D4, board.D2, board.D1]
+
+# Tell kmk we are not using a key matrix
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+# Here you define the buttons corresponding to the pins
+# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
+# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+keyboard.keymap = [
+    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),] #remap later
+]
+
+# Start kmk!
+if __name__ == '__main__':
+    keyboard.go()
