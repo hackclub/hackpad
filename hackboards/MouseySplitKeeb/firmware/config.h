34383ddb7fbcbaa53198717b9f2eb0844e48d04d
mouseylicense <nevobloch@gmail.com>
2025-03-16 13:39:19 +0200

    added mouseysplitkeeb

---
+++ hackboards/MouseySplitKeeb/firmware/config.h
@@ -0,0 +1,4 @@
+#define SPLIT_HAND_PIN GP22
+#define SPLIT_USB_DETECT
+#define MATRIX_ROW_PINS_LEFT { GP10, GP11, GP12, GP13 }  // Left-hand rows
+#define MATRIX_ROW_PINS_RIGHT { GP21, GP20, GP19, GP18 }  // Right-hand rows
