05ce6168380a5b4a3428ca89426fce8f49e8c4cd
foglomon <79741360+foglomon@users.noreply.github.com>
2025-05-28 21:46:21 +0530

    Delete hackpads/fogpad/firmware directory

--- hackpads/fogpad/firmware/rules.mk
+++
@@ -1,7 +0,0 @@
-MCU = RP2040
-BOARD = seeed_xiao_rp2040
-
-ENCODER_ENABLE = yes
-OLED_ENABLE = yes
-LTO_ENABLE = yes
-SRC += uart.c
--- hackpads/fogpad/firmware/uart.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void uart_init(void);
-bool uart_available(void);
-char uart_read(void);
