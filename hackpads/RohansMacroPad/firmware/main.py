ecd87087cb32761c05737ccdf1bfe6e4f74738e9
QuantumKya <106355292+QuantumKya@users.noreply.github.com>
2025-06-28 23:40:55 -0500

    Override crappy files with good ones HOPEFULLY LAST TIME

--- hackpads/RohansMacroPad/firmware/main.py
+++ hackpads/RohansMacroPad/firmware/main.py
@@ -6,6 +6,8 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.modules.encoder import EncoderHandler
+from kmk.extensions.rgb import RGB
 
 # This is the main instance of your keyboard
 keyboard = KMKKeyboard()
@@ -14,8 +16,21 @@ keyboard = KMKKeyboard()
 macros = Macros()
 keyboard.modules.append(macros)
 
+# Rotary Encoder implementation
+encoder_handler = EncoderHandler()
+keyboard.modules.append(encoder_handler)
+
+encoder_handler.pins = ((board.A0, board.A1),)  # A0 = GPIO26, A1 = GPIO27
+encoder_handler.map = [((KC.VOLD, KC.VOLU),)]   # Rotate left/right
+
+# LED implementation
+rgb_ext = RGB(pixel_pin=board.D4, num_pixels=1)
+keyboard.extensions.append(rgb_ext)
+
 # Define your pins here!
-PINS = [board.D3, board.D4, board.D2, board.D1]
+PINS = [board.D1, board.D2, board.D4, board.D3]
+# Encoder button
+PINS.append(board.A2)
 
 # Tell kmk we are not using a key matrix
 keyboard.matrix = KeysScanner(
@@ -27,7 +42,13 @@ keyboard.matrix = KeysScanner(
 # Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
 # And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
 keyboard.keymap = [
-    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
+    [
+        KC.A,
+        KC.DELETE,
+        KC.MACRO("Hello world!"),
+        KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),
+        KC.ENTER,
+    ]
 ]
 
 # Start kmk!
